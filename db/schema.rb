db07967a6bab35dc4c053e245c3d2b63089c4339
Ikuo Terado <eqobar@gmail.com>
2014-02-08 21:06:11 +0900

    Support user name to be used for sign up/in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140208082355) do
+ActiveRecord::Schema.define(version: 20140208095017) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -27,9 +27,11 @@ ActiveRecord::Schema.define(version: 20140208082355) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
