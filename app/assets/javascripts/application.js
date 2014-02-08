d78dc46309901881636f8c5b74b70b0804eba1ed
Ikuo Terado <eqobar@gmail.com>
2014-02-08 15:59:47 +0900

    Support focusing the current menu to be highlighted

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks
+//= require bootstrap/transition
+//= require bootstrap/collapse
 //= require turbolinks
 //= require_tree .
---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,8 @@
+'use strict';
+
+$(function () {
+  $('#menu li').click(function (e) {
+    // $('#menu').children().removeClass('active');
+    $(this).addClass('active');
+  });
+});
